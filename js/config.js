950d366553c8a58b343f68569e40bc3eeee221cf
Mouad Lazrak <lazrakmouad9@gmail.com>
2025-10-26 02:32:58 +0100

    feat: add user pool info

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_uIT6fUFs6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4e57mhk33gk8hqk4guoqu0ho24', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_ZkaqXL9Ba', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2jcfjfbf7oousi392ssqgucot', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
